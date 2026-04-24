0b0908f56863d8138319904faac72a3202c56a40
thedamfr <dam.cavailles@gmail.com>
2026-04-24 08:02:46 +0200

    chore: régénérer types Strapi 5.43 (focalPoint sur PluginUploadFile)

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1039,6 +1039,7 @@ export interface PluginUploadFile extends Struct.CollectionTypeSchema {
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     ext: Schema.Attribute.String;
+    focalPoint: Schema.Attribute.JSON;
     folder: Schema.Attribute.Relation<'manyToOne', 'plugin::upload.folder'> &
       Schema.Attribute.Private;
     folderPath: Schema.Attribute.String &
